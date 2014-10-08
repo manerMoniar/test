e4218bbe0ed81309958254e4a898a2dae241e9e6
Maner Moniar <maner.moniar@gmail.com>
2014-10-08 16:59:36 -0500

    Test

--- a.py
+++ a.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import lettuce_webdriver.webdriver  
 
+"""
 driver = webdriver.Firefox()
 driver.get("http://www.python.org")
 assert "Python" in driver.title
@@ -8,4 +10,11 @@ elem = driver.find_element_by_name("q")
 elem.send_keys("pycon")
 elem.send_keys(Keys.RETURN)
 assert "No results found." not in driver.page_source
+driver.close()
+"""
+
+driver = webdriver.Firefox()
+driver.get("http://127.0.0.1:5000/appointments/")
+elem = driver.find_element_by_css_selector("#main > div.content.container > div > div:nth-child(1) > div > h3 > a")
+assert elem.text == "Past Meeting"
 driver.close()
